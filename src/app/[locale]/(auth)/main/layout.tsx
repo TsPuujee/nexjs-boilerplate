14c4bb247aae25d60a4edec9caf1878531fca5e4
Begtse <it-head@toktok.mn>
2024-05-07 21:36:13 +0800

    :zap: fix template

--- src/app/[locale]/(auth)/main/layout.tsx
+++ src/app/[locale]/(auth)/main/layout.tsx
@@ -1,46 +1,7 @@
-import Link from "next/link";
-import { useTranslations } from "next-intl";
-
-// import LocaleSwitcher from "@/components/LocaleSwitcher";
-// import { LogOutButton } from "@/components/LogOutButton";
-import { BaseTemplate } from "@/templates/auth";
-
 export default function MainLayout(props: { children: React.ReactNode }) {
-  const t = useTranslations("MainLayout");
-
   return (
-    <BaseTemplate
-      header={
-        <>
-          <li>
-            <Link
-              href="/main/"
-              className="border-none text-gray-700 hover:text-gray-900">
-              {t("main_link")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/main/profile/"
-              className="border-none text-gray-700 hover:text-gray-900">
-              {t("user_profile_link")}
-            </Link>
-          </li>
-        </>
-      }
-      // header={
-      //   <>
-      //     <li>
-      //       <LogOutButton />
-      //     </li>
-
-      //     <li>
-      //       <LocaleSwitcher />
-      //     </li>
-      //   </>
-      // }
-    >
+    <>
       {props.children}
-    </BaseTemplate>
+    </>
   );
 }
